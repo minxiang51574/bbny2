ae5beb341e4a5689856595ff0f61f1b8877af8bd
minxiang51574 <546143220@qq.com>
2022-11-16 23:09:22 +0800

    feat:更新

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Mx
  * @Date: 2022-11-14 23:45:27
- * @Description:
+ * @Description:逻辑处理
  */
 (function ($) {
   $(document).ready(function () {
@@ -9,9 +9,33 @@
     $(".header ul li a").each(function () {
       $(this).click(function () {
         $(".header .active").removeClass("active");
-        console.log("click");
         $(this).addClass("active");
-        // return false;
+      });
+    });
+
+    $(".header .home").hover(
+      function () {
+        console.log("11");
+        $(".header .home-nav").show();
+        $(".header .navBg").show();
+        $(this).parent().addClass("on");
+      },
+      function () {
+        console.log("22");
+        $(".header .home-nav").hide();
+        $(".header .navBg").hide();
+        $(this).parent().removeClass("on");
+      }
+    );
+
+    // 关于我们 点击视频播放
+    var videopaly = $(".aboutUs .videopaly");
+    var video = $("video")[0];
+    videopaly.click(function () {
+      video.play();
+      videopaly.hide(); // 隐藏播放按钮
+      video.addEventListener("ended", function () {
+        videopaly.show(); // 播放结束 显示播放按钮
       });
     });
   });
